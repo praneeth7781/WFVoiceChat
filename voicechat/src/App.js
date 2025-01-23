85c9ff24903e834f94050de991b50b6637ca0903
Mani Praneeth Chilukuri <88399316+praneeth7781@users.noreply.github.com>
2025-01-23 14:20:05 +0530

    Add files via upload

---
+++ voicechat/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dictaphone from './components/Dictaphone';
+
+function App() {
+  return (
+    <div className="App">
+      <Dictaphone />
+    </div>
+  );
+}
+
+export default App;
